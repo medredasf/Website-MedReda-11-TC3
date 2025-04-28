72e5a996ff313133c259bbdfe72d68c23691bd35
Mmed_reda <mohammed.reda.dev@gmail.com>
2025-04-28 03:14:05 +0100

    w=!W

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
 const loginForm = document.querySelector(".login-form");
 const registerForm = document.querySelector(".register-form");
 const wrapper = document.querySelector(".wrapper");
@@ -29,3 +31,69 @@ function registerFunction(){
     registerTitle.style.top = "50%";
     registerTitle.style.opacity = 1;
 }
+=======
+const loginForm = document.querySelector(".login-form");
+const registerForm = document.querySelector(".register-form");
+const wrapper = document.querySelector(".wrapper");
+const loginTitle = document.querySelector(".title-login");
+const registerTitle = document.querySelector(".title-register");
+const signUpBtn = document.querySelector("#SignUpBtn");
+const signInBtn = document.querySelector("#SignInBtn");
+
+function loginFunction(){
+    loginForm.style.left = "50%";
+    loginForm.style.opacity = 1;
+    registerForm.style.left = "150%";
+    registerForm.style.opacity = 0;
+    wrapper.style.height = "500px";
+    loginTitle.style.top = "50%";
+    loginTitle.style.opacity = 1;
+    registerTitle.style.top = "50px";
+    registerTitle.style.opacity = 0;
+}
+
+function registerFunction(){
+    loginForm.style.left = "-50%";
+    loginForm.style.opacity = 0;
+    registerForm.style.left = "50%";
+    registerForm.style.opacity = 1;
+    wrapper.style.height = "580px";
+    loginTitle.style.top = "-60px";
+    loginTitle.style.opacity = 0;
+    registerTitle.style.top = "50%";
+    registerTitle.style.opacity = 1;
+}
+>>>>>>> d86a47f607980205ca3c5bd1de7c95c5c4dc320c
+=======
+const loginForm = document.querySelector(".login-form");
+const registerForm = document.querySelector(".register-form");
+const wrapper = document.querySelector(".wrapper");
+const loginTitle = document.querySelector(".title-login");
+const registerTitle = document.querySelector(".title-register");
+const signUpBtn = document.querySelector("#SignUpBtn");
+const signInBtn = document.querySelector("#SignInBtn");
+
+function loginFunction(){
+    loginForm.style.left = "50%";
+    loginForm.style.opacity = 1;
+    registerForm.style.left = "150%";
+    registerForm.style.opacity = 0;
+    wrapper.style.height = "500px";
+    loginTitle.style.top = "50%";
+    loginTitle.style.opacity = 1;
+    registerTitle.style.top = "50px";
+    registerTitle.style.opacity = 0;
+}
+
+function registerFunction(){
+    loginForm.style.left = "-50%";
+    loginForm.style.opacity = 0;
+    registerForm.style.left = "50%";
+    registerForm.style.opacity = 1;
+    wrapper.style.height = "580px";
+    loginTitle.style.top = "-60px";
+    loginTitle.style.opacity = 0;
+    registerTitle.style.top = "50%";
+    registerTitle.style.opacity = 1;
+}
+>>>>>>> d86a47f607980205ca3c5bd1de7c95c5c4dc320c
